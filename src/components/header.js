2c8ac42f4eedcd9420a88fc0a445b86a6c2472cb
longyiszh <longyiszh@gmail.com>
2019-05-07 13:48:30 -0400

    A header component for component import demo

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./header.css";
+
+export default class Header extends React.Component {
+
+  render() {
+    return (
+      <header>
+        <h1>Hello React v2.0</h1>
+      </header>
+    );
+  }
+
+}
