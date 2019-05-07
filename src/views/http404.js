7b4fb422fa6481c914b4b88301471dbd8b76c05e
longyiszh <longyiszh@gmail.com>
2019-05-07 13:48:45 -0400

    index page and 404 page for router demo

---
+++ src/views/http404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Http404 extends React.Component {
+
+  render() {
+    return (
+        <main className="http404">
+          404 works
+        </main>
+    );
+  }
+
+}
---
+++ src/views/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import logo from '../logo.svg';
+import "./index.css"
+
+export default class Index extends React.Component {
+
+  render() {
+    return (
+        <main className="Index">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </main>
+    );
+  }
+
+}
