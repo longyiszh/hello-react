d66c97dc88a58d66d2bc448e659f5dbafb97f9d1
longyiszh <longyiszh@gmail.com>
2019-05-08 10:46:11 -0400

    router: redirect to index on visiting "/"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
+// elements
+import Clock from "./components/clock";
+
+// views
 import Header from "./components/header";
 import Index from "./views/index";
 import Http404 from "./views/http404";
+import Registration from "./views/registration";
 
 export default class App extends React.Component {
 
@@ -13,6 +24,7 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <Header />
+        <Clock />
         <Router>
           <nav>
             <ul>
@@ -20,15 +32,23 @@ export default class App extends React.Component {
                 <Link to="/index">index</Link>
               </li>
               <li>
+                <Link to="/registration">Registration</Link>
+              </li>
+              <li>
                 <Link to="/404">404</Link>
               </li>
             </ul>
           </nav>
-          <main className="router">
-            <Route path="/" exact component={Index} />
+
+          <Switch>
+
+            <Route exact path="/" render={() => ( <Redirect to="/index"/> )}/>
             <Route path="/index" component={Index} />
-            <Route path="/404" exact component={Http404} />
-          </main>
+            <Route path="/registration" component={Registration} />
+            <Route component={Http404} />
+            
+          </Switch>
+
         </Router>
       </div>
     );
